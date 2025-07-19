290d5b0496a816cc6119d0537910a9e07fa6c792
Sanket-Kurle <sanketkurle567@gmail.com>
2025-07-20 04:47:06 +0530

    20-07-2025 at 04:47

--- question practice/gcd_lcm.java
+++ question practice/gcd_lcm.java
@@ -5,8 +5,32 @@ public class gcd_lcm{
     Scanner scn=new Scanner(System.in);
     int n1=scn.nextInt();
     int n2=scn.nextInt();
-    
-    
+    // int Divisor=n1;
+    // int Divident=n2;
+    // int temp=0;
+    // while(Divisor!=0){
+    //   temp=Divisor;
+    //   Divisor=Divident%Divisor;
+    //   Divident=temp;
+
+    // }
+    // int gcd=Divident;
+    // int lcm=(n1*n2)/gcd;
+    // System.out.println(Divident);
+    // System.out.println(lcm);
+
+    // ANOTHER APPROACH 
+    int on1=n1;
+    int on2=n2;
+    while(n1%n2!=0){
+      int rem=n1%n2;
+      n1=n2;
+      n2=rem;
+    }
+    int gcd =n2;
+    int lcm=(on1*on2)/gcd;
+    System.out.println(gcd);
+    System.out.println(lcm);
   }
 }
 
---
+++ question practice/prime_fact.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class prime_fact {
+  public static void main(String[] args) {
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+    for(int div=2;div*div<=n;div++){
+      while(n%div==0){
+        n=n/div;
+        System.out.print(div);
+      }
+    }
+    if(n!=1){
+System.out.println(n);
+    }
+  }
+}
+
+
+
+
+
+
+// Prime Factorization
+
+
+// Question Statement: 
+// 1. You are required to display the prime factorization of a number.
+// 2. Take as input a number n.
+// 3. Print all its prime factors from smallest to largest.
