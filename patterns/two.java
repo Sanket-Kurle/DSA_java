c858405fa1e86c76b3e8b5af685323ef9f52ed53
Sanket-Kurle <sanketkurle567@gmail.com>
2025-07-22 20:54:01 +0530

    22-02-2025-20:53

--- patterns/two.java
+++ patterns/two.java
@@ -6,7 +6,7 @@ public class two{
 
     for(int i=n;i>=1;i--){
       for(int j=1;j<=i;j++){
-      System.out.print('*');
+      System.out.print("*\t");
       }
       System.out.println();
     }
